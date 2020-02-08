6560b03ee48978dd66e22826cd16befcc28db5fb
Kunal Botla <kunalbotla@gmail.com>
2020-02-08 18:20:34 -0500

    Logout

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import datetime
 
 app = Flask(__name__)
+app.secret_key = 'advfysuijhyue'
 
 client = MongoClient("mongodb+srv://admin:admin@cluster0-dmgya.mongodb.net/test?retryWrites=true&w=majority")
 db = client['blueprint2020']
@@ -10,12 +11,22 @@ messages = db['messages']
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html', messages=messages.find({}))
+    return render_template('index.html', messages=messages.find({}), loggedIn='username' in session, username=session.get('username', ''))
+
+@app.route('/login', methods=['POST'])
+def login():
+    session['username'] = request.form['username']
+    return 'success'
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('username, None')
+    return 'success'
 
 @app.route('/message', methods=['POST'])
 def new_message():
     print(request.form)
-    message = request.form['message']
+    message = session['username'] + ":" + request.form['message']
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     new_object = {'message': message, 'ts': now}
     messages.insert_one(new_object)
--- static/main.js
+++ static/main.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
+  $('#login').click(function(){
+    const username = $('#username').val();
+    console.log(username);
+    $.post('/login', { //send a login request to the backend
+      username: username,
+    }).done(function() {
+      document.location.reload();
+    });
+  });
+
+  $('#logout').click(function(){
+    const message = $('#message').val();
+    $.post('/logout', {}).done(function() {
+      document.location.reload();
+
+    });
+  });
+
+
   $('#submit').click(function(){
     const message = $('#message').val();
     $.post('/message', { //send a post request to the backend
-      message: message
+    message: message
     }).done(function() {
       document.location.reload();
     });
